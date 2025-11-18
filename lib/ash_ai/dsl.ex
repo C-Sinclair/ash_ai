cd18fe749a12ae2d6058b4625e3e127b4e5fab52
Conor Sinclair <conor@sinclair.software>
2025-11-18 10:25:28 +0000

    extracted dsl from Tool module

--- lib/ash_ai/dsl.ex
+++ lib/ash_ai/dsl.ex
@@ -12,6 +12,35 @@ defmodule AshAi.Dsl do
 
   require Ash.Expr
 
+  @tool_schema [
+    name: [type: :atom, required: true],
+    resource: [type: {:spark, Ash.Resource}, required: true],
+    action: [type: :atom, required: true],
+    action_parameters: [
+      type: {:list, :atom},
+      required: false,
+      doc:
+        "A list of action specific parameters to allow for the underlying action. Only relevant for reads, and defaults to allowing `[:sort, :offset, :limit, :result_type, :filter]`"
+    ],
+    load: [
+      type: :any,
+      default: [],
+      doc:
+        "A list of relationships and calculations to load on the returned records. Note that loaded fields can include private attributes, which will then be included in the tool's response. However, private attributes cannot be used for filtering, sorting, or aggregation."
+    ],
+    async: [type: :boolean, default: true],
+    description: [
+      type: :string,
+      doc: "A description for the tool. Defaults to the action's description."
+    ],
+    identity: [
+      type: :atom,
+      default: nil,
+      doc:
+        "The identity to use for update/destroy actions. Defaults to the primary key. Set to `false` to disable entirely."
+    ]
+  ]
+
   @full_text %Spark.Dsl.Entity{
     name: :full_text,
     imports: [Ash.Expr],
@@ -83,7 +112,7 @@ defmodule AshAi.Dsl do
     option allows including private attributes in the response data.
     """,
     target: AshAi.Tools.Tool,
-    schema: AshAi.Tools.Tool.schema(),
+    schema: @tool_schema,
     args: [:name, :resource, :action]
   }
 
--- lib/ash_ai/tools/tool.ex
+++ lib/ash_ai/tools/tool.ex
@@ -16,36 +16,4 @@ defmodule AshAi.Tools.Tool do
     :action_parameters,
     __spark_metadata__: nil
   ]
-
-  @schema [
-    name: [type: :atom, required: true],
-    resource: [type: {:spark, Ash.Resource}, required: true],
-    action: [type: :atom, required: true],
-    action_parameters: [
-      type: {:list, :atom},
-      required: false,
-      doc:
-        "A list of action specific parameters to allow for the underlying action. Only relevant for reads, and defaults to allowing `[:sort, :offset, :limit, :result_type, :filter]`"
-    ],
-    load: [
-      type: :any,
-      default: [],
-      doc:
-        "A list of relationships and calculations to load on the returned records. Note that loaded fields can include private attributes, which will then be included in the tool's response. However, private attributes cannot be used for filtering, sorting, or aggregation."
-    ],
-    async: [type: :boolean, default: true],
-    description: [
-      type: :string,
-      doc: "A description for the tool. Defaults to the action's description."
-    ],
-    identity: [
-      type: :atom,
-      default: nil,
-      doc:
-        "The identity to use for update/destroy actions. Defaults to the primary key. Set to `false` to disable entirely."
-    ]
-  ]
-
-  @doc false
-  def schema, do: @schema
 end
